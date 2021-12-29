d5f7dda8fc2247a6e5aa7eb6cdb0fce529f635a9
Petr Bazurin <petr.bazurin@gmail.com>
2021-12-29 21:22:21 +0200

    Add absolute url processor tests

--- src/services/processors/absolute-url.processor.spec.ts
+++ src/services/processors/absolute-url.processor.spec.ts
@@ -20,7 +20,86 @@ describe('AbsoluteUrlProcessor', () => {
     processor = app.get<AbsoluteUrlProcessor>(AbsoluteUrlProcessor);
   });
 
-  it('should create', () => {
-    expect(processor).toBeTruthy();
+  const testRealUrl = 'http://test/real/url';
+  const generateTestUrl = (url: string) => `proxy>>${url}<<proxy`;
+  const testGetProxyUrl = async (url: string) => {
+    return Promise.resolve(generateTestUrl(url));
+  };
+
+  describe('valid urls', () => {
+    const testCases: [string, string][] = [
+      [`http://t.c/ss`, `${generateTestUrl('http://t.c/ss')}`],
+      [
+        `something https://here.com and one more here = "http://another.one/url/"`,
+        `something ${generateTestUrl(
+          'https://here.com',
+        )} and one more here = "${generateTestUrl('http://another.one/url/')}"`,
+      ],
+      [
+        `
+        something https://here.com/ and 
+        one more here = "http://another.one/url-something"
+        `,
+        `
+        something ${generateTestUrl('https://here.com/')} and 
+        one more here = "${generateTestUrl('http://another.one/url-something')}"
+        `,
+      ],
+    ];
+
+    test.each(testCases)(
+      'given %p, returns %p',
+      async (url, expectedResult) => {
+        const result = await processor.process(
+          url,
+          testRealUrl,
+          testGetProxyUrl,
+        );
+        expect(result).toEqual(expectedResult);
+      },
+    );
+  });
+
+  describe('invalid urls', () => {
+    const testCases: [string, string][] = [
+      ['', ''],
+      [`test`, `test`],
+      [`http:/test`, `http:/test`],
+      [`htt ps://some. com/else"`, `htt ps://some. com/else"`],
+    ];
+
+    test.each(testCases)(
+      'given %p, returns %p',
+      async (url, expectedResult) => {
+        const result = await processor.process(
+          url,
+          testRealUrl,
+          testGetProxyUrl,
+        );
+        expect(result).toEqual(expectedResult);
+      },
+    );
+  });
+
+  describe('excluded urls', () => {
+    const testCases: [string, string][] = [
+      ['http://www.w3.org/2000/svg', 'http://www.w3.org/2000/svg'],
+      [
+        `http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd`,
+        `http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd`,
+      ],
+    ];
+
+    test.each(testCases)(
+      'given %p, returns %p',
+      async (url, expectedResult) => {
+        const result = await processor.process(
+          url,
+          testRealUrl,
+          testGetProxyUrl,
+        );
+        expect(result).toEqual(expectedResult);
+      },
+    );
   });
 });
